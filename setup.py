44f869724cada6e23654ee938ada58ba93ce7fac
jminardi <jminardi@enthought.com>
2013-09-08 15:40:19 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 INFO = {'name': 'Robot Brain',
-        'version': '0.1.0',
+        'version': '0.2.0',
         }
-  
+
 setup(
    name = INFO['name'],
    version = INFO['version'],
